8a588b50f62fbe7aa39f88369f99d11e3c12d3a9
Nikolay Terleev <unclepepper.dev@gmail.com>
2025-04-20 22:15:29 +0300

    feat: Поправил юзера

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -49,7 +49,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-class User implements UserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
 
     use TimestampableEntity;
